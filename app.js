5d0ad74b5106952cafa18497788255c015587959
Qasim <qalhamad33@gmail.com>
2024-06-30 23:15:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+/*-------------------------------- Constants --------------------------------*/
+const display = document.querySelector(".display");
+let buttons = document.querySelectorAll(".button");
+/*-------------------------------- Variables --------------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+/*----------------------------- Event Listeners -----------------------------*/
+/*-------------------------------- Functions --------------------------------*/
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let number = event.target.textContent;
+    if (number !== "=" && number !== "C") {
+      if (display.textContent == "" && display.textContent == true) {
+      } else {
+        display.textContent += number;
+      }
+    }
+    if (number == "=" && number !== "C") {
+      display.textContent = eval(display.textContent);
+    }
+    if (number == "C") {
+      display.textContent = "";
+    }
+  });
+});
