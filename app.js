9ebc33c54b3b0c24d5a0575bd06f9e5889c93ed0
Qasim <qalhamad33@gmail.com>
2024-06-30 23:50:11 +0300

    third commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     let number = event.target.textContent;
     if (number !== "=" && number !== "C") {
-      if (display.textContent == "" && display.textContent == true) {
+      if (display.textContent === "" && display.textContent === true) {
       } else {
         display.textContent += number;
       }
